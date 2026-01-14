8f3211b13b00e886a7e77a67c6f2a28078a5c3e0
Dishambha Awasthi <111968535+dishambha@users.noreply.github.com>
2026-01-15 00:39:54 +0530

    Time: 0 ms (100%), Space: 20 MB (5.79%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.py
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.py
@@ -1,16 +1,20 @@
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        n = len(mat)
-        m = len(mat[0])
-        res =[]
-        for i in range(0,n):
-            for j in range(0,m):
-                res.append(mat[i][j])
-        if n*m != r*c:
-            return mat
-        final_res =[]
-        for i in range(0,len(res),c):
-            sol = res[i:i+c]
-            final_res.append(sol)
+        m = len(mat)
+        n = len(mat[0])
+        flat_mat =[]
+        final_res = []
+        for i in range(0,m):
+            for j in range(0,n):
+                flat_mat.append(mat[i][j])
 
+        if m*n != c*r:
+            return mat
+        leng = len(flat_mat)
+        res =[]
+        for i in range(0,leng,c):
+            res = flat_mat[i:i+c]
+            final_res.append(res)
         return final_res
+
+
