3c5f953ecbff9fbb065d19efab10cb0522173fd9
Dishambha Awasthi <111968535+dishambha@users.noreply.github.com>
2026-02-04 22:13:26 +0530

    Time: 63 ms (20.83%), Space: 39.6 MB (45.24%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        rem_index = {0:-1}
+        pref_sum=0
+        for i in range(n):
+            pref_sum += nums[i]
+            rem = pref_sum%k
+            if rem in rem_index:
+                if i-rem_index.get(rem,0) >= 2:
+                    return True
+            else:
+                rem_index[rem] = rem_index.get(rem,0)+i
+        return False
